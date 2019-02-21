5274b3659ce07842faf77b11ca29894ef4f57bf8
nkfoss <nathankfoss@gmail.com>
2019-02-21 14:29:45 -0600

    Create todo-list.component.ts

---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,82 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListService} from './todo-list.service';
+import {Todo} from './todo';
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+  providers: []
+})
+
+export class UserListComponent implements OnInit {
+  // These are public so that tests can reference them (.spec.ts)
+  public todos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoOwner: string;
+  public todoStatus: string;
+
+
+  // Inject the UserListService into this component.
+  // That's what happens in the following constructor.
+  //
+  // We can call upon the service for interacting
+  // with the server.
+
+  constructor(private todoListService: TodoListService) {
+
+  }
+
+  public filterTodos(searchOwner: string, searchStatus: boolean): Todo[] {
+
+    this.filteredTodos = this.todos;
+
+    // Filter by name
+    if (searchOwner != null) {
+      searchOwner = searchOwner.toLocaleLowerCase();
+
+      this.filteredTodos = this.filteredTodos.filter(todo => {
+        return !searchOwner || todo.owner.toLowerCase().indexOf(searchOwner) !== -1;
+      });
+    }
+
+    // Filter by age
+    if (searchStatus != null) {
+      this.filteredTodos = this.filteredTodos.filter((todo: Todo) => {
+        return !searchStatus || (todo.status === searchStatus);
+      });
+    }
+
+    return this.filteredTodos;
+  }
+
+  /**
+   * Starts an asynchronous operation to update the users list
+   *
+   */
+  refreshTodos(): Observable<Todo[]> {
+    // Get Users returns an Observable, basically a "promise" that
+    // we will get the data from the server.
+    //
+    // Subscribe waits until the data is fully downloaded, then
+    // performs an action on it (the first lambda)
+
+    const todos: Observable<Todo[]> = this.todoListService.getTodos();
+    todos.subscribe(
+      returnedTodos => {
+        this.todos = returnedTodos;
+        this.filterTodos(this.todoOwner, this.todoStatus);
+      },
+      err => {
+        console.log(err);
+      });
+    return todos;
+  }
+
+
+  ngOnInit(): void {
+    this.refreshTodos();
+  }
+}
